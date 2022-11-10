c373c005da3be1e0f9cb22fd4ea505200904acca
Janinegwapa <janineetang83@gmail.com>
2022-11-10 02:26:08 -0800

    added by etang

--- registration.php
+++ registration.php
@@ -12,7 +12,7 @@
 				<div class="card-body">
 					<h3 class="card-title text-center">Sign Up</h3>
 					<div class="card-text">
-					<form action="" method="POST">
+					<form action="" method="POST" enctype="multipart/form-data">
 						<div style="text-align: center;">
 							<label>Enter your fullname:</label>
 							<input type="text" name="fullname" class="form-control form-control-sm">
@@ -27,6 +27,10 @@
 							<label>Password:</label>
 							<input type="password" class="form-control form-control-sm" name="password">
 						</div>
+						<div style="text-align: center;">
+							<label>Upload image:</label>
+							<input type="file" name="fileToUpload" id="fileToUpload">
+						</div>
 						<br>
 						<div style="text-align: center;">
 							<button type="submit" class="btn btn-primary btn-block">Save</button>
@@ -39,16 +43,44 @@
 </body>
 </html>
 <?php
-	include("model/User.php");
+	include("model/users.php");
 	require("connection/database.php");
 
 	if (isset($_POST['fullname'])) {
 		$fullname = $_POST['fullname'];
 		$username = $_POST['username'];
 		$password = hash('md5',$_POST['password']);
+		$image = "";
+
+		$target_dir = "uploads/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+			// Check if image file is a actual image or fake image
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+		    // echo "File is an image - " . $check["mime"] . ".";
+		    // $uploadOk = 1;
+		} else {
+		    echo "File is not an image.";
+		    $uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+		  echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+		} else {
+		  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		    // echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+		    $image = $target_file;
+		  } else {
+		    echo "Sorry, there was an error uploading your file.";
+		  }
+		}
 		
-		$saveUser = new User($conn);
-		if ($saveUser->store($fullname, $username,$password)) {
+		
+		$saveUser = new Users($conn);
+		if ($saveUser->store($fullname, $username, $password, $image)) {
 			echo "<script>alert('Successfully added!')</script>";
 		}
 	}
