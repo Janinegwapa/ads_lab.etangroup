7cfaf4c2dfe66fb9a3b848332e8c10efee8d5b93
Janinegwapa <janineetang83@gmail.com>
2022-11-10 02:19:20 -0800

    added by etang

---
+++ model/users.php
@@ -0,0 +1,20 @@
+<?php
+class Users {
+
+  // Methods
+    public function __construct($conn){
+        $this->conn = $conn;
+    }
+
+    public function store($fullname, $username, $password, $image) {
+
+      $sql = "INSERT INTO users (fullname, username, password, image) VALUES('$fullname', '$username', '$password', '$image')";
+
+      if ($this->conn->query($sql) === TRUE) {
+      return true;
+      } else {
+          return false;
+      }
+    }
+}
+?>
